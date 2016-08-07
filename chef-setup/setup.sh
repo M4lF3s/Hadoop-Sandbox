b483ba34be2a94b3d776ed1c29c81f4c7912eafa
Marcel Fraas <mfraas64@gmail.com>
2016-08-07 18:00:35 +0200

    Added setup Script
    Setup Shell-Script added;
    Moved chef-files to subfolder „chef-setup/chef“

---
+++ chef-setup/setup.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# install necessery tools
+yum install -y gcc-c++ patch \
+      readline readline-devel \
+      zlib zlib-devel libyaml-devel \
+      libffi-devel openssl-devel make \
+      bzip2 autoconf automake libtool \
+      bison iconv-devel libyaml \
+      svn
+
+
+# install Ruby + Chef
+cd /tmp
+
+wget ftp://ftp.ruby-lang.org/pub/ruby/2.0/ruby-2.0.0-p247.tar.gz
+tar -zxvf ruby-2.0.0-p247.tar.gz
+cd ruby-2.0.0-p247
+./configure --prefix=/usr/local
+
+make
+make install
+
+gem install chef ruby-shadow --no-ri --no-rdoc
+
+
+# get the Chef-Cookbook
+mkdir /var/chef
+svn checkout https://github.com/mfraas64/Hadoop-Sandbox/branches/chef-setup/chef-setup/chef /var/chef
+
+
+# change to the chef Directory and execute chef-solo
+cd /var/chef
+chef-solo -c solo.rb
