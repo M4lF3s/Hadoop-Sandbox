ddef22b75fdfa2faf7179e272eb29767203a335b
Marcel Fraas <mfraas64@gmail.com>
2016-08-11 23:22:34 +0200

    Fixed Setup Script
    Removed cookbooks folder from setup;
    Added ambari-cookbook to Cheffile

--- chef-setup/setup.sh
+++ chef-setup/setup.sh
@@ -7,7 +7,7 @@ yum install -y gcc-c++ patch \
       libffi-devel openssl-devel make \
       bzip2 autoconf automake libtool \
       bison iconv-devel libyaml \
-      svn rubygems ruby-devel wget
+      rubygems ruby-devel git svn
 
 
 # add the hostname to /etc/hosts
@@ -16,7 +16,7 @@ echo 127.0.0.1 $HOSTNAME >> /etc/hosts
 
 # install Ruby + Chef
 cd /tmp
-wget ftp://ftp.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz
+curl -O ftp://ftp.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz
 tar -zxvf ruby-2.3.1.tar.gz
 cd ruby-2.3.1
 ./configure --prefix=/usr/local
@@ -30,13 +30,16 @@ mkdir /var/chef
 cd /var/chef
 gem install librarian-chef --no-ri --no-rdoc
 librarian-chef init
+
+
 # echo "cookbook 'ambari', '~> 0.2.2'" >> Cheffile
-echo "cookbook 'apt', '~> 4.0.1'" >> Cheffile
-echo "cookbook 'compat_resource', '~> 12.10.7'" >> Cheffile
+#echo "cookbook 'apt', '~> 4.0.1'" >> Cheffile
+#echo "cookbook 'compat_resource', '~> 12.10.7'" >> Cheffile
+echo "cookbook 'ambari', :git => 'https://github.com/jp/ambari'"
 librarian-chef install
 
 
-# get the Custom-Cookbooks from Repository
+# ceckout chef-branch
 svn checkout https://github.com/mfraas64/Hadoop-Sandbox/branches/chef-setup/chef-setup/chef /var/chef
 
 
